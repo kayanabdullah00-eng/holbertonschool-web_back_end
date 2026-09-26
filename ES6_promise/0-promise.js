535cc3ecacf25d8eba2a10a2c3a3b19a0a932c32
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-09-26 12:36:11 +0300

    feat: add getResponseFromAPI promise function

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+/**
+ * Returns a Promise.
+ * @returns {Promise} - A new Promise object.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Promise implementation
+  });
+}
